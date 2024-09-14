808b064f19746d061d33d542c57ba939c48715fe
kkayam <korre.remix@gmail.com>
2024-09-14 12:49:50 +0300

    test google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import GoogleAnalytics from "./GoogleAnalytics";
 
 export default function App({ Component, pageProps }) {
   return <>
     <Component {...pageProps} />
-    <Analytics />
+    <GoogleAnalytics />
   </>;
 }
